26838e504a9a5f97b531663e5323fbb2fa560aef
matejadb <92729211+matejadb@users.noreply.github.com>
2024-12-14 11:58:57 +0100

    test

--- app.js
+++ app.js
@@ -34,14 +34,36 @@ function operate(first_number, second_number, operator) {
 let first_number = undefined;
 let second_number = undefined;
 let operator = undefined;
-
+let result = 0;
+let end = -1;
 // SELECTIONS
 const container = document.querySelector(".container");
 const output = document.querySelector(".output");
-const numbers = document.querySelectorAll(".numbers");
+const number = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operators");
 
 // FUNCTIONALITY
-numbers.forEach((number) => {
+number.forEach((number) => {
 	number.addEventListener("click", (e) => {});
 });
+
+/*
+if (first_number === undefined) {
+			first_number = e.target.innerText;
+			output.innerText = e.target.innerText;
+			first_number = parseInt(first_number);
+			console.log(typeof first_number);
+		} else if (operator === undefined && first_number != undefined) {
+			operator = e.target.innerText;
+			console.log(first_number + " " + operator);
+		} else if (
+			second_number === undefined &&
+			operator !== undefined &&
+			first_number !== undefined
+		) {
+			second_number = e.target.innerText;
+			output.innerText = e.target.innerText;
+			second_number = parseInt(second_number);
+			console.log(first_number + " " + operator + " " + second_number);
+		}
+*/
