2d46099cb3d6b15b9cf4182644af84bdf0e52a31
matejadb <92729211+matejadb@users.noreply.github.com>
2024-12-14 14:10:16 +0100

    Trying to figure out logic

--- app.js
+++ app.js
@@ -30,41 +30,58 @@ function operate(first_number, second_number, operator) {
 	}
 }
 
+function getFirstNumber() {
+	number.forEach((number) => {
+		number.addEventListener("click", (e) => {
+			first_number = parseInt(e.target.innerText);
+			output.innerText = first_number;
+			console.log("First number: " + first_number);
+		});
+	});
+}
+
+function getSecondNumber() {
+	number.forEach((number) => {
+		number.addEventListener("click", (e) => {
+			second_number = parseInt(e.target.innerText);
+			output.innerText = second_number;
+			console.log("Second number: " + second_number);
+		});
+	});
+}
+
+function getOperator() {
+	operators.forEach((operator) => {
+		operator.addEventListener("click", (e) => {
+			operator = e.target.innerText;
+			output.innerText = operator;
+			console.log("Operator: " + operator);
+			operatorClicked = true;
+		});
+	});
+}
+
+function calculator() {
+	first_number = getFirstNumber();
+	getOperator();
+	if (operatorClicked) {
+		getSecondNumber();
+	}
+}
 // VARIABLES
 let first_number = undefined;
 let second_number = undefined;
 let operator = undefined;
+let operatorClicked = false;
 let result = 0;
 let end = -1;
 
 // SELECTIONS
 const container = document.querySelector(".container");
 const output = document.querySelector(".output");
-const number = document.querySelectorAll(".numbers");
-const operators = document.querySelectorAll(".operators");
+const number = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
 
 // FUNCTIONALITY
-number.forEach((number) => {
-	number.addEventListener("click", (e) => {});
-});
-
-/*
-if (first_number === undefined) {
-			first_number = e.target.innerText;
-			output.innerText = e.target.innerText;
-			first_number = parseInt(first_number);
-			console.log(typeof first_number);
-		} else if (operator === undefined && first_number != undefined) {
-			operator = e.target.innerText;
-			console.log(first_number + " " + operator);
-		} else if (
-			second_number === undefined &&
-			operator !== undefined &&
-			first_number !== undefined
-		) {
-			second_number = e.target.innerText;
-			output.innerText = e.target.innerText;
-			second_number = parseInt(second_number);
-			console.log(first_number + " " + operator + " " + second_number);
-		}
-*/
+calculator();
+//getOperator();
