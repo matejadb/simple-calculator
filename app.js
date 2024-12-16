55ee54f958a36cf1c251f4e495b6b8657c52952a
matejadb <92729211+matejadb@users.noreply.github.com>
2024-12-16 17:52:12 +0100

    Add keyboard support (not finished)

--- app.js
+++ app.js
@@ -148,6 +148,27 @@ keys.addEventListener("click", (e) => {
 	}
 });
 
+document.addEventListener("keydown", (e) => {
+	if (
+		e.key === "1" ||
+		e.key === "2" ||
+		e.key === "3" ||
+		e.key === "4" ||
+		e.key === "5" ||
+		e.key === "6" ||
+		e.key === "7" ||
+		e.key === "8" ||
+		e.key === "9" ||
+		e.key == "0"
+	) {
+		const displayedNum = display.textContent;
+		if (displayedNum === "0") {
+			display.textContent = e.key;
+		} else {
+			display.textContent = displayedNum + e.key;
+		}
+	}
+});
 /*
 TODO:
 - KEYBOARD SUPPORT
